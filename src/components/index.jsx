af1579dfe0db5b856f543a3f55b145f294833fd6
Mandar Waghe <mandar.waghe@udgama.com>
2022-05-29 04:58:49 +0530

    initial commit file structure

---
+++ src/components/index.jsx
@@ -0,0 +1,16 @@
+export { default as Button } from "./Button";
+export { default as ThemeSettings } from "./ThemeSettings";
+export { default as Sidebar } from "./Sidebar";
+export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
+export { default as Cart } from "./Cart";
+export { default as Chat } from "./Chat";
+export { default as Notifications } from "./Notifications";
+export { default as UserProfile } from "./UserProfile";
+export { default as ChartsHeader } from "./ChartsHeader";
+export { default as Header } from "./Header";
+
+export { default as Bar } from "./Charts/Bar";
+export { default as LineCharts } from "./Charts/LineCharts";
+export { default as Pie } from "./Charts/Pie";
+export { default as Stacked } from "./Charts/Stacked";
